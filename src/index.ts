82d8afe4c858eeff940103d3688629e82002e527
Aman07a <aman16.aa17@gmail.com>
2023-03-30 11:23:37 +0200

    Why is This Bad?

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 class Sorter {
-  constructor(public collection: number[] | string) {}
+  constructor(public collection: number[] | string | LinkedList) {}
 
   sort(): void {
     const { length } = this.collection;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection instanceof LinkedList) {
+          //
+        }
+
         // If collection is an array of numbers
         if (this.collection instanceof Array) {
           if (this.collection[j] > this.collection[j + 1]) {
